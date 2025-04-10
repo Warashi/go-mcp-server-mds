52a11346d2cd6a6eb585873137069d3038f7093d
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-04-11 05:53:44 +0900

    feat: add exclude-frontmatter option to server command-line flags
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

--- cmd/mcp-server-mds/main.go
+++ cmd/mcp-server-mds/main.go
@@ -5,18 +5,20 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 
 	mcpmds "github.com/Warashi/go-mcp-server-mds"
 )
 
 func main() {
-	var path, name, description string
+	var path, name, description, excludeFrontmatter string
 	flag.StringVar(&path, "path", ".", "path to the directory to serve")
 	flag.StringVar(&name, "name", "mcp-server-mds", "name of the server")
 	flag.StringVar(&description, "description", "Markdown Documents Server", "description of the server")
+	flag.StringVar(&excludeFrontmatter, "exclude-frontmatter", "", "comma-separated list of keys to exclude from frontmatter")
 	flag.Parse()
 
-	server, err := mcpmds.New(name, description, os.DirFS(path))
+	server, err := mcpmds.New(name, description, os.DirFS(path), mcpmds.WithExcludeFrontmatter(strings.Split(excludeFrontmatter, ",")...))
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
 	}
